0e0495bd8fd00aecfe220173794ffebde3966d51
李文辉 <407325196@qq.com>
2019-04-29 22:31:54 +0800

    ac

--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -14,7 +14,7 @@ class Base extends Controller
 	public function _initialize()
     {
         $this->session_id = session_id();
-        header("Cache-control: private");  // history.back返回后输入框值丢失问题 参考文章 http://www.tp-shop.cn/article_id_1465.html  http://blog.csdn.net/qinchaoguang123456/article/details/29852881
+//        header("Cache-control: private");  // history.back返回后输入框值丢失问题 参考文章 http://www.tp-shop.cn/article_id_1465.html  http://blog.csdn.net/qinchaoguang123456/article/details/29852881
         define('SESSION_ID',$this->session_id); //将当前的session_id保存为常量，供其它方法调用
         //获取导航栏
         $nav = get_goods_category_tree();
